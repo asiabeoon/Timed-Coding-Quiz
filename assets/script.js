f3c906907c4c17979c5903e44145ffd594b32609
asiabeoon <a.hashbey@gmail.com>
2023-04-08 14:32:26 -0400

    added index,js, pages and starter code

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var startButton = document.getElementsByClassName("start")[0]
+
+var questionOne = document.getElementsByClassName("question1")[0]
+var questionTwo = document.getElementsByClassName("question2")[0]
+var questionThree =document.getElementsByClassName("question3")[0]
+var questionFour=document.getElementsByClassName("question4")[0]
+
+
+
+startButton.addEventListener("click", function(){
+    alert("button was clicked")
+});
